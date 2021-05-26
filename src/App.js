f8325cf905eb407358d79466b8bd0225a84d0f68
Matthew Cantelon <gh@matthewcantelon.ca>
2021-05-26 10:34:21 -0700

    feat: adding chakra provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 
 import Navigation from './components/nav.js'
 
 import Home from './pages/home.js';
 import Companies from './pages/companies.js';
 import Jobs from './pages/jobs.js';
-import History from './pages/history.js'
 import Error from './pages/error.js';
 import Help from './pages/help.js'
 
@@ -15,26 +15,28 @@ class App extends Component {
 
   render() {
     return (
-       <BrowserRouter>
-        <div>
-          <Navigation />
-            <Switch>
-             <Route path="/" render={props =>
-               (<Home {...props} state={this.state}/>)
-             } exact/>
-             <Route path="/jobs" render={props =>
-               (<Jobs {...props} state={this.state}/>)
-             } exact/>
-             <Route path="/companies" render={props =>
-               (<Companies {...props} state={this.state}/>)
-             } exact/>
+      <ChakraProvider theme={theme}>
+         <BrowserRouter>
+          <div>
+            <Navigation />
+              <Switch>
+               <Route path="/" render={props =>
+                 (<Home {...props} state={this.state}/>)
+               } exact/>
+               <Route path="/jobs" render={props =>
+                 (<Jobs {...props} state={this.state}/>)
+               } exact/>
+               <Route path="/companies" render={props =>
+                 (<Companies {...props} state={this.state}/>)
+               } exact/>
 
-             <Route path="/help" component={Help}/>
+               <Route path="/help" component={Help}/>
 
-             <Route component={Error}/>
-           </Switch>
-        </div>
-      </BrowserRouter>
+               <Route component={Error}/>
+             </Switch>
+          </div>
+        </BrowserRouter>
+      </ChakraProvider>
     );
   }
 }
